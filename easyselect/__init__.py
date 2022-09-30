a789bf02fc4eca44d18a0b4d0bc40f48261812b7
gmanka <gmankab@gmail.com>
2022-09-30 21:49:39 +0400

    aboba

--- easyselect/__init__.py
+++ easyselect/__init__.py
@@ -9,14 +9,16 @@ from rich import traceback
 from rich.console import Console
 from pynput import keyboard
 import subprocess
+import platform
 import sys
 
-traceback.install()
+traceback.install(show_locals=True)
 pretty.install()
 console = Console()
 print = console.print
 run = subprocess.getstatusoutput
 Key = keyboard.Key
+system = platform.system()
 
 
 class Selection:
@@ -25,10 +27,13 @@ class Selection:
         items: list | tuple,
         styles: list | tuple = [],
         chosen: int = 0,
+        page_size: int = 10,
     ) -> None:
         self.items = items
         self.chosen = chosen
+        self.page_size = page_size
         self.len = len(items)
+        self.start = 0
         if not styles:
             styles = [None] * len(self.items)
         self.styles = styles
@@ -38,15 +43,29 @@ class Selection:
             self.chosen = self.len - 1
         elif self.chosen >= self.len:
             self.chosen = 0
+        if self.chosen < self.start:
+            self.start = self.chosen
+        end = self.start + self.page_size
+        if self.chosen >= end:
+            end = self.chosen + 1
+            self.start = end - self.page_size
+        to_print = self.items[
+            self.start : end
+        ]
 
-        for index, item in enumerate(self.items):
+        for index, item in enumerate(to_print):
+            index = self.start + index
             if index == self.chosen:
-                item = f'[blue]➜[/blue]  [reverse]{item}[/reverse]'
+                if system == 'Windows':
+                    item = f'[blue]➜[/blue]   [reverse]{item}[/reverse]'
+                else:
+                    item = f'[blue]➜[/blue]  [reverse]{item}[/reverse]'
             else:
                 item = f'    {item}'
             print(
                 item,
-                style = self.styles[index]
+                style = self.styles[index],
+                highlight = False,
             )
         print()
 
@@ -57,23 +76,41 @@ class Selection:
             (
                 up_one + erase_line
             ) * (
-                self.len + 1
+                min(self.page_size, self.len) + 1
             )
         )
         self.print()
 
     def choose(self):
-        def on_press(pressed_key):
-            match pressed_key:
+        def on_press(key):
+            if 'char' in key.__dict__.keys():
+                key = key.char
+            match key:
                 case Key.esc:
                     self.chosen = None
                     return False
                 case Key.enter:
                     return False
-                case Key.up:
+                case Key.up | Key.left | 'w' | 'a':
                     self.chosen -= 1
-                case Key.down:
+                case Key.down | Key.right | 's' | 'd':
                     self.chosen += 1
+                case Key.page_up:
+                    self.chosen -= self.page_size
+                    self.chosen = max(
+                        self.chosen,
+                        0
+                    )
+                case Key.page_down:
+                    self.chosen += self.page_size
+                    self.chosen = min(
+                        self.chosen,
+                        self.len - 1
+                    )
+                case Key.home:
+                    self.chosen = 0
+                case Key.end:
+                    self.chosen = self.len - 1
             self.update()
 
         run('stty -echo')
